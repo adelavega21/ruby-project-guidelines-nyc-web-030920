f378199f0ed41355e8af3a9b66d4d14d36e8f4df
Asilver-jpg <asilver7493@gmail.com>
2020-03-23 15:00:37 -0400

    establish api connection

---
+++ lib/get_request.rb
@@ -0,0 +1,22 @@
+require 'net/http'
+require 'open-uri'
+require 'json'
+require 'pry'
+
+
+class GetRequest
+    URL =  "http://www.omdbapi.com/?i=tt3896198&apikey=5c915f2f"
+   def get_data
+        uri = URI.parse(URL)
+        response = Net::HTTP.get_response(uri).body
+   end
+
+   def read_data
+    data = JSON.parse(get_data)
+  
+   end
+   
+end
+
+g = GetRequest.new.read_data
+binding.pry
